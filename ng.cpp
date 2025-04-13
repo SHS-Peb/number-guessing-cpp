d78cb2adf9cf469396d28027c227233d77145e6d
unknown <blackskiesdancer@gmail.com>
2025-04-13 22:33:22 +1000

    random index initialised

--- ng.cpp
+++ ng.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 using namespace std;
 
 int main (){
   int userGuess;
   int computerGuess;
+
+  int randomIndex = rand() % 3;
+  computerGuess = randomIndex;
 
   cout << "Guess a number between 1 and 10";
   cin >> userGuess;
