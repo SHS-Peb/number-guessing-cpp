fc9dc4e63aec846a911283844d6fd85974c87da6
unknown <blackskiesdancer@gmail.com>
2025-04-13 22:45:53 +1000

    corrected random logic

--- ng.cpp
+++ ng.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
-#include <string>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main (){
   int userGuess;
   int computerGuess;
 
-  int randomIndex = rand() % 3;
-  computerGuess = randomIndex;
+  srand(time(0));
+  computerGuess = (rand() % 10) + 1;
 
-  cout << "Guess a number between 1 and 10";
+  cout << "Guess a number between 1 and 10: ";
   cin >> userGuess;
 
     if (userGuess == computerGuess) {
         cout << "You guessed it!" << endl;
     } else {
-        cout << "Nope! The number was" << computerGuess << endl;
+        cout << "Nope! The number was " << computerGuess << endl;
     }
 
     return 0; 
